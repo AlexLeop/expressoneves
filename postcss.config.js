696ac87751dfe213d3b0593984baa39dcc64270c
Alex Leopoldo da Silva <lx.leopoldo@outlook.com>
2025-07-15 15:04:27 -0300

    Restaurar sistema completo com correções

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: {
-    tailwindcss: tailwindcss,
-    autoprefixer: autoprefixer,
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 }
