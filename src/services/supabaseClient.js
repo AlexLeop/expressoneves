bcb235bb3bf955e91fc93f4f7f0c55e5331a44fc
Alex Leopoldo da Silva <lx.leopoldo@outlook.com>
2025-07-17 15:38:14 -0300

    Fix Supabase API key configuration

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://exfljeezwtnidjsiynei.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZmxqZWV6d3RuaWRqc2l5bmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NjA3NzUsImV4cCI6MjA1MzIzNjc3NX0.c8xn6l6o0PBKgJ-lO8cgG8TsItzM3Qf9pHGhZSWJQWs';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
